422db3c3965e27089a4bee28ccd49106febea178
SuryaChintapalli <suryach312@gmail.com>
2026-06-20 16:55:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.3 MB (86.66%)

---
+++ 0046-permutations/solution.py
@@ -0,0 +1,7 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        from itertools import permutations
+        perm=permutations(nums)
+        return(list(perm))
+        
+        
