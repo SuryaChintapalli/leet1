2f6faee85c56a5e1426b726bb1c17cbaeec31bea
SuryaChintapalli <suryach312@gmail.com>
2026-06-20 16:58:25 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 19.5 MB (77.41%)

---
+++ 0074-search-a-2d-matrix/solution.py
@@ -0,0 +1,23 @@
+from typing import List
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        beg = 0
+        end = rows * cols - 1
+
+        while beg <= end:
+            mid = (beg + end) // 2
+            i = mid // cols
+            j = mid % cols
+
+            if matrix[i][j] == target:
+                return True
+            elif target < matrix[i][j]:
+                end = mid - 1
+            else:
+                beg = mid + 1
+
+        return False
+        
